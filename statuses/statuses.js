b79b41edca0250bdb84dec7531ffe1d9f0bf8050
Violaceous2 <aoi.ribbon@gmail.com>
2017-06-02 18:09:31 +0900

    Status effects
    added several for now

--- statuses/statuses.js
+++ statuses/statuses.js
@@ -17,7 +17,7 @@ Game.StatusEffects.poison = function(multi){
 	this.name = 'poison';
 	this.value = {
 		regenBonus: -2 - (multi || 0),
-		strength: -1 - (multi || 1)
+		strength: *.90 - (multi || 0),
 		};
 	this.duration = 5 * (multi || 1);
 
@@ -27,10 +27,59 @@ Game.StatusEffects.poison = function(multi){
 
 Game.StatusEffects.weakness = function(multi){
 	this.name = 'weakness';
-	this.value = {strength: -3 - (multi || 0)};
-	this.duration = 10 + (multi || 1);
+	this.value = {strength: *.30 - (multi || 0)
+	};
+	this.duration = 6 + (multi || 1);
 }
 
+Game.StatusEffects.blindness = function(multi){
+	this.name = 'blindness';
+	this.value = {sightRadius: -3 - (multi || 1),
+		perception: *.30),
+		dexterity: *.50),
+	};
+	this.duration = 7 + (multi || 1);
+}
+
+Game.StatusEffects.daze = function(multi){
+	this.name = 'daze';
+	this.value = {strength: *.80 - (multi || 0),
+		perception: *.70),
+		speed: *.60),
+	};
+	this.duration = 4 + (multi || 1);
+}
+
+Game.StatusEffects.bless = function(multi){
+	this.name = 'bless';
+	this.value = {strength: *1.30 - (multi || 0),
+		dexterity: *1.30),
+		vitality: *1.40),
+	};
+	this.duration = 9 + (multi || 1);
+}
+
+Game.StatusEffects.haste = function(multi){
+	this.name = 'haste';
+	this.value = {speed: *1.60 - (multi || 0)
+	};
+	this.duration = 6 + (multi || 1);
+}
+
+Game.StatusEffects.chilled = function(multi){
+	this.name = 'chilled';
+	this.value = {speed: *.80 - (multi || 0)
+		strength: *.80
+	};
+	this.duration = 5 + (multi || 1);
+}
+
+Game.StatusEffects.slow = function(multi){
+	this.name = 'slow';
+	this.value = {speed: *1.60 - (multi || 0)
+	};
+	this.duration = 5 + (multi || 1);
+}
 
 
 //this function is used to generate a status and apply it to a target
