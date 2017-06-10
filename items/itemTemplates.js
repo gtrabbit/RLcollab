b745b5ca86c34a6464479cf27a306ea336ba6d6a
Violaceous2 <aoi.ribbon@gmail.com>
2017-06-11 03:08:44 +0900

    lots of stuff
    and things

--- items/itemTemplates.js
+++ items/itemTemplates.js
@@ -5,46 +5,216 @@
 
 //====================Weapons ===========>>
 
-Game.Items.Equipment.Weapons.Dagger = {
-
+//daggers
+Game.Items.Equipment.Weapons.Daggers = {
+	dagger: {
 	character: ")",
+	foregroundColor: 'grey',
 	attackValue: 5,
 	name: 'dagger',
 	weight: 4,
 	variance: 1,
 	bonuses: {   //possible to modify secondary stats here as well. Could do primary, but that would be weird if the standard sword raised your strength. But I am thinking that daggers might be easier to swing, so could have them give higher doubleswing chance, etc. for any other items and so on
-		DoubleSwing: 2
+		DoubleSwing: 17
+		MeleeCritical: 3
 	}
 	
 	
 }
+},
+
+//swords
+Game.Items.Equipment.Weapons.Sword = {
+	shortsword: {
+	character: ")",
+	foregroundColor: 'white',
+	attackValue: 8,
+	name: 'short sword,'
+	weight: 4,
+	variance: 3,
+	bonuses: {   //possible to modify secondary stats here as well. Could do primary, but that would be weird if the standard sword raised your strength. But I am thinking that daggers might be easier to swing, so could have them give higher doubleswing chance, etc. for any other items and so on
+		DoubleSwing: 8
+		MeleeCritical: 1
+	}
+},
 
 
-Game.Items.Equipment.Weapons.Axe = {
-	attackValue: 12,
-	name: 'axe',
-	weight: 14,
-	variance: 10
 
+	longsword: {
+	character: ")",
+	foregroundColor: 'white',
+	attackValue: 12
+	name: 'long sword',
+	weight: 9,
+	variance: 5,
+	bonuses: {   //possible to modify secondary stats here as well. Could do primary, but that would be weird if the standard sword raised your strength. But I am thinking that daggers might be easier to swing, so could have them give higher doubleswing chance, etc. for any other items and so on
+	}
+	
+	
+},
 }
 
+//staves
+Game.Items.Equipment.Weapons.Staves = {
+	yewstaff: {
+	character: ")",
+	foregroundColor: 'blue',
+	attackValue: 6,
+	name: 'yew staff,'
+	weight: 6,
+	variance: 3,
+	bonuses: {   //possible to modify secondary stats here as well. Could do primary, but that would be weird if the standard sword raised your strength. But I am thinking that daggers might be easier to swing, so could have them give higher doubleswing chance, etc. for any other items and so on
+		Arcana: 2
+	}
+},
+}
 
+//axes
+Game.Items.Equipment.Weapons.Axes = {
+	handaxe: {
+	character: ")",
+	foregroundColor: 'brown',
+	attackValue: 4,
+	name: 'hand axe',
+	weight: 5,
+	variance: 4,
+	bonuses: {
+		MeleeCriticalDamageBonus: 2
+		MeleeCritical: 1
+		}
+},
+
+	broadaxe: {
+	character: ")",
+	foregroundColor: 'brown,'
+	attackValue: 9
+	name: 'broad axe,'
+	weight: 4,
+	variance: 8
+	bonuses: {   //possible to modify secondary stats here as well. Could do primary, but that would be weird if the standard sword raised your strength. But I am thinking that daggers might be easier to swing, so could have them give higher doubleswing chance, etc. for any other items and so on
+		MeleeCriticalDamageBonus: 4
+		MeleeCritical: 2
+		}
+},
+	
+	
+}
+
+//================ Armor========>>
+Game.Items.Equipment.Armor.Body = {
+	hide: {
+	character: "#",
+	foregoundcolor: 'turquoise,'
+	defenseValue: 1,
+	name: "hide armor",
+	weight: 2
+},
+	
+	leather: {
+	character: "#",
+	foregoundcolor: 'turquoise,'
+	defenseValue: 3,
+	name: "leather armor",
+	weight: 5
+},
+
+
+	chainmail: {
+	character: "#",
+	foregoundcolor: 'turquoise,'
+	defenseValue: 7,
+	name: "chainmail",
+	weight: 16
+	bonuses: {
+	speed: -200
+	DoubleSwing: -8
+	}
+},
+
+	platemail: {
+	character: "#",
+	foregoundcolor: 'turquoise,'
+	defenseValue: 14,
+	name: "platemail",
+	weight: 38
+	bonuses: {
+	speed: -400
+	DoubleSwing: -25
+	}
+}
+},
 
 //================ Helmets =========>>
 
-Game.Items.Equipment.Helmets.Cap = {
+Game.Items.Equipment.Armor.Helmets = {
+	cap: {
 	character: "^",
 	defenseValue: 2,
 	name: "cap",
 	weight: 7
 }
+},
 
 //================ Bracers =========>>
 
-Game.Items.Equipment.Bracers.Bracers = {
+Game.Items.Equipment.Armor.Bracers = {
+	bracer: {
 	character: "8",
 	defenseValue: 1,
 	name: "bracer",
 	weight: 3
+},
+	
+	gauntlets: {
+	character: "8",
+	defenseValue: 3,
+	name: "gauntlets",
+	weight: 5
 }
+},
 
+//================ Boots========>>
+Game.Items.Equipment.Armor.Boots = {
+	boots: {
+	character: ",",
+	foregroundColor: 'brown',
+	defenseValue: 1,
+	name: "boots",
+	weight: 2
+}
+},
+
+//================ Rings=====>>
+Game.Items.Equipment.Armor.Rings = {
+	ring: {
+	character: ".",
+	foregroundColor: 'gold',
+	defenseValue: 0,
+	name: "ring",
+	weight: 0
+}
+},
+
+//================ Amulets=====>>
+Game.Items.Equipment.Armor.Amulets = {
+	amulet: {
+	character: ".",
+	foregroundColor: 'pink',
+	defenseValue: 1,
+	name: "amulet",
+	weight: 0
+}
+},
+//=========== Capes ========>>
+
+Game.Items.Equipment.Armor.Capes = {
+	cape: {
+	character: "["
+	defenseValue: 0,
+	name: "cape",
+	weight: 1
+	bonuses: {
+	Evasion: 10
+	}
+}
+},
--- items/prefixesANDsuffixes.js
+++ items/prefixesANDsuffixes.js
@@ -5,34 +5,220 @@
 
 
 
-Game.Items.Prefix.Material.Silver = {
+Game.Items.WeaponPrefix.Material.Silver = {
 	prefix: "silver",
 	baseStatBonus: 3, //is applied to attack for weapons, defense for armor
-	rarity: 20,
-	weight: 2,   //multiplies base by this amount
+	rarity: 18,
+	weight: 2, //multiplies by this amount
 	modifies: {  //everything in here multiplied by multi (from quality) and then added to template base
-		dexterity: 1, 
-		DoubleSwing: 2 //if you do a secondary stat, format the name exactly as it appears after the "get" on its function, so, in this case, capitalize the first word as well, like in this example
+	MeleeCriticalDamageBonus: 4
+	MagicSlayer: 3
+	//if you do a secondary stat, format the name exactly as it appears after the "get" on its function, so, in this case, capitalize the first word as well, like in this example
 }
 }
 
+Game.Items.ArmorPrefix.Material.Silver = {
+	prefix: "silver",
+	baseStatBonus: 3, //is applied to attack for weapons, defense for armor
+	rarity: 18,
+	weight: 2, //multiplies by this amount
+	modifies: {  //everything in here multiplied by multi (from quality) and then added to template base
+	MagicalResist: 16
+	//if you do a secondary stat, format the name exactly as it appears after the "get" on its function, so, in this case, capitalize the first word as well, like in this example
+}
+}
+
+Game.Items.WeaponPrefix.Material.Iron = {
+	prefix: "iron",
+	basestatBonus: 1,
+	rarity: 40,
+	weight: 2.2,
+	modifies: {
+	MeleeCriticalDamageBonus: 2,
+	
+}
+}
+
+Game.Items.ArmorPrefix.Material.Iron = {
+	prefix: "iron",
+	basestatBonus: 1,
+	rarity: 40,
+	weight: 2.2,
+	modifies: {
+	ResistPhysical: 10
+	
+}
+}
+
+Game.Items.WeaponPrefix.Material.Steel = {
+	prefix: "steel",
+	basestatBonus: 3,
+	rarity: 18,
+	weight: 1.5,
+	modifies: {
+	MeleeCriticalDamageBonus: 4,
+		
+}
+}
+
+Game.Items.ArmorPrefix.Material.Steel = {
+	prefix: "steel",
+	basestatBonus: 3,
+	rarity: 18,
+	weight: 1.5,
+	modifies: {
+		
+}
+}
+
+Game.Items.WeaponPrefix.Material.Elven = {
+	prefix: "elven",
+	basestatBonus: 2,
+	rarity: 5,
+	weight: 0.6,
+	modifies: {
+	MeleeCriticalDamageBonus: 2,
+	DoubleSwing: 5,
+}
+}
+
+Game.Items.ArmorPrefix.Material.ElvenA = {
+	prefix: "elven",
+	basestatBonus: 2,
+	rarity: 5,
+	weight: 0.6,
+	modifies: {
+	speed: 100,
+}
+}
 
 
 //=========================QUALITY===========>>>>>>
 
-Game.Items.Prefix.Quality.Good = {
+Game.Items.ArmorPrefix.Quality.Good = {
 	prefix: "good",
-	rarity: 70,
-	multi: 1.5  //all relevant attributes multiplied by this number
+	rarity: 60,
+	multi: 1.5, //all relevant attributes multiplied by this number
 			//these three are the only relevant values for this prefix type
 }
 
+Game.Items.WeaponPrefix.Quality.Good = {
+	prefix: "good",
+	rarity: 60,
+	multi: 1.5, //all relevant attributes multiplied by this number
+			//these three are the only relevant values for this prefix type
+}
 
-Game.Items.Prefix.Quality.Cheap = {
+Game.Items.ArmorPrefix.Quality.Formidable = {
+	prefix: "formidable",
+	rarity: 45,
+	multi: 1.75,
+	
+}
+
+Game.Items.WeaponPrefix.Quality.Formidable = {
+	prefix: "formidable",
+	rarity: 45,
+	multi: 1.75,
+	
+}
+
+Game.Items.ArmorPrefix.Quality.Cheap = {
 	prefix: "cheap",
 	multi: 0.4,
-	rarity: 100  
+	rarity: 70, 
 
+}
+
+Game.Items.WeaponPrefix.Quality.heap = {
+	prefix: "cheap",
+	multi: 0.4,
+	rarity: 70, 
+
+}
+
+Game.Items.ArmorPrefix.Quality.Shoddy = {
+	prefix: "shoddy",
+	multi: 0.7,
+	rarity: 70,
+
+}
+
+Game.Items.WeaponPrefix.Quality.Shoddy = {
+	prefix: "shoddy",
+	multi: 0.7,
+	rarity: 70,
+
+}
+
+Game.Items.ArmorPrefix.Quality.Exquisite = {
+	prefix: "exquisite",
+	multi: 3,
+	rarity: 20,
+
+}
+
+Game.Items.WeaponPrefix.Quality.Exquisite = {
+	prefix: "exquisite",
+	multi: 3,
+	rarity: 20,
+
+}
+
+Game.Items.ArmorPrefix.Quality.Masterwork = {
+	prefix: "masterwork",
+	multi: 3.5,
+	rarity: 10,
+
+}
+
+Game.Items.WeaponPrefix.Quality.Masterwork = {
+	prefix: "masterwork",
+	multi: 3.5,
+	rarity: 10,
+
+}
+
+Game.Items.ArmorPrefix.Quality.Glorious = {
+	prefix: "glorious",
+	multi: 2.5,
+	rarity: 30,
+
+}
+
+Game.Items.WeaponPrefix.Quality.Glorious = {
+	prefix: "glorious",
+	multi: 2.5,
+	rarity: 30,
+
+}
+
+Game.Items.ArmorPrefix.Quality.Miraculous = {
+	prefix: "miraculous",
+	multi: 4,
+	rarity: 5,
+	
+}
+
+Game.Items.WeaponPrefix.Quality.Miraculous = {
+	prefix: "miraculous",
+	multi: 4,
+	rarity: 5,
+	
+}
+
+Game.Items.ArmorPrefix.Quality.Godlike = {
+	prefix: "godlike",
+	multi: 5,
+	rarity: 1,
+	
+}
+
+Game.Items.WeaponPrefix.Quality.Godlike = {
+	prefix: "godlike",
+	multi: 5,
+	rarity: 1,
+	
 }
 
 
@@ -41,55 +227,138 @@ Game.Items.Prefix.Quality.Cheap = {
 
 
 
-Game.Items.Prefix.Classy.Knight = {
-	prefix: "knight's",  //notice that these do not need to line up with how it's declared above
+Game.Items.WeaponPrefix.Classy.Squire = {
+	prefix: "squire's", //notice that these do not need to line up with how it's declared above
 	modifies: {
 		vitality: 2,
 		strength: 3,
-		AccuracyBonus: 20
+		AccuracyBonus: 20,
 	},
-	rarity: 10
+	rarity: 25,
 }
 
-Game.Items.Prefix.Classy.Thief = {
-	prefix: "theif's",  //notice that these do not need to line up with how it's declared above
+Game.Items.ArmorPrefix.Classy.Squire = {
+	prefix: "squire's", //notice that these do not need to line up with how it's declared above
+	modifies: {
+		vitality: 2,
+		strength: 3,
+		DefenseValue: 2,
+	},
+	rarity: 25,
+}
+
+Game.Items.WeaponPrefix.Classy.Thief = {
+	prefix: "theif's", 
 	modifies: {
 		dexterity: 3,
-		perception: 2,
-		MeleeCritical: 3,
+		MeleeCritical: 2,
 	},
-	rarity: 10
+	rarity: 25,
 }
 
-Game.Items.Prefix.Classy.Mage = {
-	prefix: "mage's",  //notice that these do not need to line up with how it's declared above
+Game.Items.ArmorPrefix.Classy.Thief = {
+	prefix: "theif's", 
+	modifies: {
+		dexterity: 3,
+		evasion: 15,
+	},
+	rarity: 25,
+}
+
+Game.Items.WeaponPrefix.Classy.Mage = {
+	prefix: "mage's", 
 	modifies: {
 		intelligence: 3,
-		arcana: 2,
 		SpellCritical: 3,
 	},
-	rarity: 10
+	rarity: 25,
 }
 
-Game.Items.Prefix.Classy.Ninja = {
+Game.Items.ArmorPrefix.Classy.Mage = {
+	prefix: "mage's", 
+	modifies: {
+		intelligence: 2,
+		arcana: 4,
+	},
+	rarity: 25,
+}
+
+Game.Items.WeaponPrefix.Classy.Illusionist = {
+	prefix: "illusionist's",  
+	modifies: {
+		arcana: 6,
+		Evasion: 25
+		MagicResist: 15
+	},
+	rarity: 12,
+}
+
+Game.Items.ArmorPrefix.Classy.illusionist = {
+	prefix: "illusionist's",  
+	modifies: {
+		arcana: 6,
+		Evasion: 25
+		MagicResist: 15
+	},
+	rarity: 12,
+}
+
+
+Game.Items.WeaponPrefix.Classy.Classy.Ninja = {
 	prefix: "ninja's",
 	modifies: {
-		speed: 150
-		perception: 4,
-		dexterity: 2,
+		perception: 4
+		DoubleSwing: 8,
+		ThrowStat: 10,
 	},
-	rarity: 8
+	rarity: 15,
+}
+
+Game.Items.ArmorPrefix.Classy.Ninja = {
+	prefix: "ninja's",
+	modifies: {
+		speed: 100
+		perception: 4,
+		dexterity: 4,
+		ThrowStat: 10,
+	},
+	rarity: 15,
 }
 
 //========================ADJECTIVE ==============>>>
 
-Game.Items.Prefix.Adjective.Sharp = {
+Game.Items.WeaponPrefix.Adjective.Sharp = {
 	prefix: "sharp",
-	attackValue: 5,
-	rarity: 5,
-	weaponOnly: true,
+	rarity: 15,
 	modifies: {
-		MeleeCritical: 5  //if you do a secondary stat, format the name exactly as it appears after the "get" on its function, so, in this case, capitalize the first word as well, like in this example
+		MeleeCritical: 3,  //if you do a secondary stat, format the name exactly as it appears after the "get" on its function, so, in this case, capitalize the first word as well, like in this example
+	}
+}
+
+Game.Items.WeaponPrefix.Adjective.Deadly = {
+	prefix: "deadly",
+	rarity: 8,
+	modifies: {
+		MeleeCritical: 5,  //if you do a secondary stat, format the name exactly as it appears after the "get" on its function, so, in this case, capitalize the first word as well, like in this example
+	}
+}
+
+Game.Items.WeaponPrefix.Adjective.Furious = {
+	prefix: "furious",
+	rarity: 15,
+	modifies: {
+		DoubleSwing: 10
+		strength: 3
+	}
+}
+
+Game.Items.ArmorPrefix.Adjective.Vigorous = {
+	prefix: "vigorous",
+	rarity: 12,
+	modifies: {
+		MaxHP: 10,
+		vitality: 2
+		RegenBonus: 1,
 	}
 }
 
@@ -98,5 +367,4 @@ Game.Items.Prefix.Adjective.Spiked = {
 	attackValue: 4,
 	defenseValue: 3,
 	rarity: 12,
-	armorOnly: true
 }
