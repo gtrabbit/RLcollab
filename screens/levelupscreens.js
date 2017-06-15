ab87c941c0be0ef9e9f61068e32955fbdbb3e368
Chris Rune <gtrabbit@gmail.com>
2017-06-15 15:06:39 -0400

    option to save points

--- screens/levelupscreens.js
+++ screens/levelupscreens.js
@@ -50,16 +50,23 @@ Game.Screen.levelAbilitiesScreen = {
         this._options = Object.keys(this._abilities);
     },
     render: function(display){
-        const letters = 'abcdefghijklmnopqrstuvwxyz';
+        this.letters = 'abcdefghijklmnopqrstuvwxyz';
         display.drawText(0, 0, 'Choose an ability to increase: ');
         // Iterate through each of our options
         for (var i = 0; i < this._options.length; i++) {
-            display.drawText(0, 2 + i, 
-                letters.substring(i, i + 1) + ' - ' + this._options[i] + "  || Current Level: " + this._abilities[this._options[i]].level + " -- To Next Level: " + this._abilities[this._options[i]].toNext + "  || Total " + this._abilities[this._options[i]].totalPoints);
+            if (typeof this._abilities[this._options[i]].level === "number"){
+                display.drawText(0, 2 + i, 
+                this.letters.substring(i, i + 1) + ' - ' + this._options[i] + "  || Current Level: " + this._abilities[this._options[i]].level + " -- To Next Level: " + this._abilities[this._options[i]].toNext + "  || Total " + this._abilities[this._options[i]].totalPoints);
+            } else {
+                this.letters = "*" + this.letters;
+                display.drawText(0, 2 + i, 
+                this.letters.substring(i, i + 1) + " - " + this._options[i] + "  || Current Level: " + this._abilities[this._options[i]].level + " -- To Next Level: N/A" + "  || Total " + this._abilities[this._options[i]].totalPoints);
+            }
         }
+        display.drawText(0, 3+this._options.length, " 0 -- Save points until next level.")
 
         // Render remaining stat points
-        display.drawText(0, 4 + this._options.length,
+        display.drawText(0, 5 + this._options.length,
             "Remaining points: " + this._entity.getAbilityPoints());   
     },
     handleInput: function(inputType, inputData) {
@@ -69,7 +76,7 @@ Game.Screen.levelAbilitiesScreen = {
                 // Check if it maps to a valid item by subtracting 'a' from the character
                 // to know what letter of the alphabet we used.
                 var index = inputData.keyCode - ROT.VK_A;
-                if (this._options[index]) {
+                if (this._options[index] && this.letters[index] !== "*") {
                     this._entity.increaseAbility(this._options[index], 1);
                     this._entity.setAbilityPoints(this._entity.getAbilityPoints() - 1);
                     this.setup(this._entity)
@@ -78,7 +85,9 @@ Game.Screen.levelAbilitiesScreen = {
                     } else {
                         Game.refresh();
                     }
-                }
+                } 
+            } else if (inputData.keyCode === ROT.VK_0 || inputData.keyCode === ROT.VK_ESCAPE){
+                Game.Screen.playScreen.setSubScreen(null);
             }
         }
     }
